25e468c84d3c46cfb9192cdd8147ec96a0094e65
FernandoBruno <fernandomatheusbruno@hotmail.com>
2023-03-09 20:17:57 -0300

    First Commit

---
+++ ChatGPT/ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Fernando Matheus Ferreira Adomaitis Bruno on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
